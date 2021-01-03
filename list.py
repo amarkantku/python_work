a640a9f4492b755ce703c75ed6b9421b6a5dadaa
Amarkant Kumar <amarkant.kumar@walmartlabs.com>
2021-01-03 22:19:22 +0530

    list opt

---
+++ list.py
@@ -0,0 +1,23 @@
+''' List is a ordered collection data type in python, it is dynamic and mutable in nature'''
+
+cars = ['suzuki', 'ford', 'maruti']
+print(cars);
+
+# append a new car in list
+cars.append('honda')
+print(cars);
+
+# insert to a specific position
+cars.insert(1,'tata');
+print(cars);
+
+#remove item from list
+del cars[0]
+print(cars)
+
+car = cars.pop() # remove form last index
+print(cars)
+
+cars.remove('ford')
+print(cars)
+
